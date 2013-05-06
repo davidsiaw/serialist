425ba59e8e8569420028f70c789cbbad85aad73e
David Siaw <davidsiaw@gmail.com>
2013-05-06 17:28:53 +1200

    add verbose flag to make check tests

--- tests/test2.sh
+++ tests/test2.sh
@@ -1,4 +1,4 @@
 # this tests that the H source produced has the right define
 NAME=`../serialist/serialist h $srcdir/test.format`
-echo name = $NAME
+[[ $VERBOSE == 1 ]] && echo name = $NAME
 [[ $NAME == "#ifndef TEST_H"* ]] && exit 0 || exit 1
